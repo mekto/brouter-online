f479eadcaf13b0c653b70c9e5cb936d8929c4820
Tomasz Krzyszczyk <tomasz.krzyszczyk@gmail.com>
2015-11-16 21:35:56 +0100

    new style for route line

--- js/map.js
+++ js/map.js
@@ -1,4 +1,15 @@
 import Leaflet from 'leaflet';
 
-export default
-  new Leaflet.Map('map', {zoomControl: false, attributionControl: false});
+const map = new Leaflet.Map('map', {zoomControl: false, attributionControl: false});
+
+map.on('zoomend', function() {
+  const container = map._container;
+  const zoom = map.getZoom();
+  const match = /zoom-\d+/.exec(container.className);
+  if (match) {
+    container.classList.remove(match[0]);
+  }
+  container.classList.add(`zoom-${zoom}`);
+});
+
+export default map;
--- js/utils/RouteLayer.js
+++ js/utils/RouteLayer.js
@@ -5,7 +5,10 @@ export default Leaflet.FeatureGroup.extend({
   initialize(geojson, color) {
     const layers = [
       Leaflet.geoJson(geojson, {
-        style: () => ({color, weight: 5, opacity: 0.85, dashArray: [5, 12]})
+        style: () => ({className: 'route-line-0'})
+      }),
+      Leaflet.geoJson(geojson, {
+        style: () => ({color, className: 'route-line'})
       }),
     ];
     Leaflet.FeatureGroup.prototype.initialize.call(this, layers);
